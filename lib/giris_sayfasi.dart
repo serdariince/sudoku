2154f8863174359c73ab5e0235b5be87474e3cd9
Serdar İnce <serdariince@gmail.com>
2021-06-27 00:40:53 +0300

    Son

--- lib/giris_sayfasi.dart
+++ lib/giris_sayfasi.dart
@@ -2,6 +2,7 @@ import 'package:flutter/material.dart';
 import 'package:google_fonts/google_fonts.dart';
 import 'package:hive/hive.dart';
 import 'package:sudoku/dil.dart';
+import 'package:sudoku/sudoku_sayfasi.dart';
 
 class GirisSayfasi extends StatefulWidget {
   const GirisSayfasi({Key? key}) : super(key: key);
@@ -11,7 +12,9 @@ class GirisSayfasi extends StatefulWidget {
 }
 
 class _GirisSayfasiState extends State<GirisSayfasi> {
+  late Box _sudokuKutu;
   Future<Box> _kutuAc() async {
+   _sudokuKutu= await Hive.openBox('sudoku');
     return await Hive.openBox('tamamlanan_sudokular');
   }
 
@@ -23,7 +26,7 @@ class _GirisSayfasiState extends State<GirisSayfasi> {
         title: Text(dil['giris_title']),
         actions: <Widget>[
           IconButton(
-            icon: Icon(Icons.history_edu),
+            icon: Icon(Icons.star_border_purple500_outlined),
             onPressed: () {
               Box kutu = Hive.box('ayarlar');
               kutu.put(
@@ -32,6 +35,25 @@ class _GirisSayfasiState extends State<GirisSayfasi> {
               );
             },
           ),
+          PopupMenuButton(
+            icon: Icon(Icons.add),
+            onSelected: (deger){
+              if (_sudokuKutu.isOpen) _sudokuKutu.put('seviye', deger);
+              Navigator.push(context, MaterialPageRoute(builder: (_)=>SudokuSayfasi()));
+            },
+            itemBuilder: (context)=><PopupMenuEntry>[
+              PopupMenuItem(
+                  value: dil["seviye_sec"],
+                  child: Text( dil["seviye_sec"]),
+                  textStyle: TextStyle(fontWeight: FontWeight.bold, color: Theme.of(context).textTheme.bodyText1!.color),
+                  enabled: false,
+                  ),
+              for(String key in sudokuSeviyeleri.keys)
+                PopupMenuItem(
+                  value:key,
+                  child: Text( key),
+                  )
+          ])
         ],
       ),
       body: FutureBuilder<Box>(
--- lib/sudoku_sayfasi.dart
+++ lib/sudoku_sayfasi.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:hive/hive.dart';
 import 'package:sudoku/dil.dart';
 
 class SudokuSayfasi extends StatefulWidget {
@@ -7,14 +8,25 @@ class SudokuSayfasi extends StatefulWidget {
   @override
   _SudokuSayfasiState createState() => _SudokuSayfasiState();
 }
-
+  final Map sudokuSeviyeleri={
+dil["seviye1"]:62,
+dil["seviye2"]:53,
+dil["seviye3"]:44,
+dil["seviye4"]:35,
+dil["seviye5"]:26,
+dil["seviye6"]:17
+ 
+  };
 class _SudokuSayfasiState extends State<SudokuSayfasi> {
+
+
   @override
   Widget build(BuildContext context) {
        return Scaffold(
       appBar: AppBar(
         title:Text(dil['sudoku_title']),
       ),
+      body: Center(child: Text(Hive.box('sudoku').get('seviye',defaultValue: "Kolay"), ),),
     );
   }
 }
